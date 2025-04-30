99857ce0383bde17ef9a6298a3a33495edf4c593
Tiana-K08 <tetkononenko2020@gmail.com>
2025-04-30 13:21:10 +0200

    added exercise

--- exercise.py
+++ exercise.py
@@ -1,2 +1,11 @@
 # Cree una clase de Python llamada Usuario que use el método init y cree un nombre de usuario y una contraseña.
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
 # Crea un objeto usando la clase.
+user1 = User('Tiana', 'test1234')
+
+print(user1.username)
+print(user1.password)
